e688cb636b87aea14eb2a42de5e953c238095829
gustavo <kg.2k.pl@gmail.com>
2025-08-21 06:42:16 -0400

    feat(order): add error notifications with react-toastify in OrderSummary

--- components/order/OrderSummary.tsx
+++ components/order/OrderSummary.tsx
@@ -6,6 +6,7 @@ import { useMemo } from "react"
 import { formatCurrency } from "@/src/utils"
 import { createOrder } from "@/actions/create-order-action"
 import { OrderSchema } from "@/src/schema"
+import { toast } from "react-toastify"
 
 export default function OrderSummary() {
 
@@ -17,7 +18,11 @@ export default function OrderSummary() {
             name: formData.get('name')
         }
         const result = OrderSchema.safeParse(data)
-        console.log(result)
+        if (!result.success) {
+            result.error.issues.forEach((issue) => {
+                toast.error(issue.message)
+            })
+        }
         return
         createOrder()
     }
