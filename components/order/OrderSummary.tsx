bd552cad36d469aa4462ede3cec22acbf3f7a3f5
gustavo <kg.2k.pl@gmail.com>
2025-08-21 06:04:28 -0400

    feat(order): add button to confirm order in order summary

--- components/order/OrderSummary.tsx
+++ components/order/OrderSummary.tsx
@@ -14,7 +14,7 @@ export default function OrderSummary() {
         <aside className="lg:h-screen lg:overflow-y-scroll md:w-64 lg:w-96 p-5">
             <h1 className="text-4xl text-center font-black">Mi Pedido</h1>
 
-            {order.length === 0 ? <p className="text-center my-10">El carrito está vacío</p> : (
+            {order.length === 0 ? <p className="text-center my-10">El pedido está vacío</p> : (
                 <div className="mt-5">
                     {order.map(item => (
                         <ProductDetails key={item.id} item={item} />
@@ -24,6 +24,12 @@ export default function OrderSummary() {
                         Total a pagar: {''}
                         <span>{formatCurrency(total)}</span>
                     </p>
+
+                    <form className="w-full mt-10 space-y-5">
+                        <input type="submit" value='Confirmar Pedido'
+                            className="py-2 rounded uppercase text-white bg-black w-full text-center cursor-pointer font-bold"
+                        />
+                    </form>
                 </div>
             )}
         </aside>
