b3463e858fc844551f9df8196b67b47042e9ea71
gustavo <kg.2k.pl@gmail.com>
2025-08-22 07:01:29 -0400

    feat(order): add display of pending orders with OrderCard component

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -1,3 +1,4 @@
+import OrderCard from "@/components/order/OrderCard";
 import Heading from "@/components/ui/Heading";
 import { prisma } from "@/src/lib/prisma";
 
@@ -22,6 +23,15 @@ export default async function OrdersPage() {
     const orders = await getPendingOrders()
 
     return (
-        <Heading>Administrar Ordenes</Heading>
+        <>
+            <Heading>Administrar Ordenes</Heading>
+            {orders.length ? (
+                <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-5 mt-5">
+                    {orders.map(order => (
+                        <OrderCard key={order.id} order={order} />
+                    ))}
+                </div>
+            ) : <p>No hay ordenes pendientes</p>}
+        </>
     )
 }
