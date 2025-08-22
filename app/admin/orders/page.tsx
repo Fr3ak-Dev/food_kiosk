fcdb56e11194e277f0ee69926abd5e45a409ca6e
gustavo <kg.2k.pl@gmail.com>
2025-08-22 06:44:34 -0400

    feat(order): add a function to retrieve pending orders

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -1,6 +1,26 @@
 import Heading from "@/components/ui/Heading";
+import { prisma } from "@/src/lib/prisma";
 
-export default function OrdersPage() {
+async function getPendingOrders() {
+    const orders = await prisma.order.findMany({
+        where: {
+            status: false
+        },
+        include: {
+            orderProducts: {
+                include: {
+                    product: true
+                }
+            }
+        }
+    })
+    return orders
+}
+
+export default async function OrdersPage() {
+
+    const orders = await getPendingOrders()
+
     return (
         <Heading>Administrar Ordenes</Heading>
     )
