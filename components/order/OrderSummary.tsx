f76b6662e432c11f9ee86b1473a911423d00ccb4
gustavo <kg.2k.pl@gmail.com>
2025-08-21 21:21:13 -0400

    feat(order): add function to clear order after successful creation

--- components/order/OrderSummary.tsx
+++ components/order/OrderSummary.tsx
@@ -11,6 +11,7 @@ import { toast } from "react-toastify"
 export default function OrderSummary() {
 
     const order = useStore((state) => state.order)
+    const clearOrder = useStore((state) => state.clearOrder)
     const total = useMemo(() => order.reduce((total, item) => total + (item.quantity * item.price), 0), [order])
 
     const handleCreateOrder = async (formData: FormData) => {
@@ -33,6 +34,8 @@ export default function OrderSummary() {
             })
 
         }
+        toast.success('Pedido realizado correctamente')
+        clearOrder()
     }
 
     return (
