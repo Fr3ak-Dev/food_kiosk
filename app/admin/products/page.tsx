9e3219b67092940ea83bd2824cf4715f4e1812c5
gustavo <kg.2k.pl@gmail.com>
2025-08-23 06:20:01 -0400

    feat(admin): add product pagination and count total products

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,6 +1,11 @@
+import ProductsPagination from '@/components/products/ProductsPagination'
 import ProductTable from '@/components/products/ProductTable'
 import Heading from '@/components/ui/Heading'
 import { prisma } from '@/src/lib/prisma'
+
+async function productCount() {
+    return await prisma.product.count()
+}
 
 async function getProducts(page: number, pageSize: number) {
     const skip = (page - 1) * pageSize
@@ -20,12 +25,15 @@ export default async function ProductsPage({ searchParams }: { searchParams: { p
 
     const page = +searchParams.page || 1
     const pageSize = 10
-    const products = await getProducts(page, pageSize)
+    const productsData = getProducts(page, pageSize)
+    const totalProductsData = productCount()
+    const [products, totalProducts] = await Promise.all([productsData, totalProductsData])
 
     return (
         <>
             <Heading>Administrar Productos</Heading>
             <ProductTable products={products} />
+            <ProductsPagination page={page} />
         </>
     )
 }
