6c1f6cbfa731a5c926de0418eb470c95139e1e69
gustavo <kg.2k.pl@gmail.com>
2025-08-23 07:13:12 -0400

    feat(admin): validate the search

--- components/products/ProductSearchForm.tsx
+++ components/products/ProductSearchForm.tsx
@@ -1,6 +1,29 @@
+"use client"
+
+import { SearchSchema } from "@/src/schema"
+import { useRouter } from "next/navigation"
+import { toast } from "react-toastify"
+
 export default function ProductSearchForm() {
+
+    const router = useRouter()
+    const handleSearchForm = (formData: FormData) => {
+        const data = {
+            search: formData.get('search')
+        }
+        const result = SearchSchema.safeParse(data)
+        if (!result.success) {
+            result.error.issues.forEach(issue => {
+                toast.error((issue.message));
+
+            })
+            return
+        }
+        router.push(`/admin/products/search?search=${result.data.search}`)
+    }
+
     return (
-        <form className="flex items-center">
+        <form action={handleSearchForm} className="flex items-center">
             <input type="text" placeholder="Buscar producto" name="search"
                 className="p-2 placeholder-gray-400 w-full" />
             <input type="submit" value={'Buscar'}
