d9d79207eeeb4575f410c5af6d07a6a6dc03756b
gustavo <kg.2k.pl@gmail.com>
2025-08-23 06:49:21 -0400

    feat(admin): add pagination by numbers

--- components/products/ProductsPagination.tsx
+++ components/products/ProductsPagination.tsx
@@ -6,6 +6,8 @@ type ProductsPaginationProps = {
 }
 
 export default function ProductsPagination({ page, totalPages }: ProductsPaginationProps) {
+
+    const pages = Array.from({ length: totalPages }, (_, i) => i + 1)
     return (
         <nav className="flex justify-center py-10">
             {page > 1 && (
@@ -15,6 +17,14 @@ export default function ProductsPagination({ page, totalPages }: ProductsPaginat
                         focus:z-20 focus:outline-offset-0"
                 >&laquo;</Link>
             )}
+
+            {pages.map(currentPage => (
+                <Link href={`/admin/products?page=${currentPage}`}
+                    className={`${page === currentPage && 'font-black'} bg-white px-4 py-2 text-sm text-gray-900 ring-1 ring-inset ring-gray-300
+                        focus:z-20 focus:outline-offset-0`}
+                >{currentPage}</Link>
+            ))}
+
             {page < totalPages && (
                 <Link
                     href={`/admin/products?page=${page + 1}`}
