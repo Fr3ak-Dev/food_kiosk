34384f5d2df82f9a5f3f97e8d4cf2fa01e0f8610
gustavo <kg.2k.pl@gmail.com>
2025-08-22 23:21:30 -0400

    feat(admin): add pagination with take and skip of prisma

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -2,14 +2,25 @@ import ProductTable from '@/components/products/ProductTable'
 import Heading from '@/components/ui/Heading'
 import { prisma } from '@/src/lib/prisma'
 
-async function getProducts() {
-    const products = await prisma.product.findMany()
+async function getProducts(page: number, pageSize: number) {
+    const skip = (page - 1) * pageSize
+    const products = await prisma.product.findMany({
+        take: pageSize,
+        skip,
+        include: {
+            category: true
+        }
+    })
     return products
 }
 
-export default async function ProductsPage() {
+export type ProductsWithCategory = Awaited<ReturnType<typeof getProducts>>
 
-    const products = await getProducts()
+export default async function ProductsPage({ searchParams }: { searchParams: { page: string } }) {
+
+    const page = +searchParams.page || 1
+    const pageSize = 10
+    const products = await getProducts(page, pageSize)
 
     return (
         <>
