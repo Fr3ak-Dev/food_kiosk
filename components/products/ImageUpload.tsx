919fdb8cb630b12e9fc5f39b35add89d930f7898
gustavo <kg.2k.pl@gmail.com>
2025-08-25 17:53:01 -0400

    feat(admin): add react-icons and create image upload component with Cloudinary

--- components/products/ImageUpload.tsx
+++ components/products/ImageUpload.tsx
@@ -1,5 +1,28 @@
+"use client"
+import { CldUploadWidget } from "next-cloudinary"
+import { TbPhotoPlus } from "react-icons/tb"
+
 export default function ImageUpload() {
     return (
-        <div>ImageUpload</div>
+        // <CldUploadWidget signatureEndpoint="<API Endpoint (ex: /api/sign-cloudinary-params)>">
+        <CldUploadWidget uploadPreset="upload_food_kiosk"
+            options={{ maxFiles: 1 }}
+            onSuccess={(result, { widget }) => {
+                console.log(result)
+            }}>
+            {({ open }) => (
+                <>
+                    <div className="space-y-2">
+                        <label className="text-slate-800">Imagen Producto</label>
+                        <div className="relative cursor-pointer hover:opacity-70 transition p-10 border-neutral-300 flex flex-col justify-center items-center
+                            gap-4 text-neutral-600 bg-slate-100"
+                            onClick={() => open()}>
+                            <TbPhotoPlus size={50} />
+                            <p>Agregar imagen</p>
+                        </div>
+                    </div>
+                </>
+            )}
+        </CldUploadWidget>
     )
 }
