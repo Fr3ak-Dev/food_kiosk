7544d4552eb7bf138e742f99d4dabb63ab888d55
gustavo <kg.2k.pl@gmail.com>
2025-08-25 23:57:12 -0400

    feat(admin): Add image field and handle product creation on the form

--- components/products/AddProductForm.tsx
+++ components/products/AddProductForm.tsx
@@ -1,16 +1,21 @@
 "use client"
 
+import { createProduct } from '@/actions/create-product-action'
 import { ProductSchema } from '@/src/schema'
+import { useRouter } from 'next/navigation'
 import React from 'react'
 import { toast } from 'react-toastify'
 
 export default function AddProductForm({ children }: { children: React.ReactNode }) {
 
+    const router = useRouter()
+
     const handleSubmit = async (formData: FormData) => {
         const data = {
             name: formData.get('name'),
             price: formData.get('price'),
-            categoryId: formData.get('categoryId')
+            categoryId: formData.get('categoryId'),
+            image: formData.get('image')
         }
         const result = ProductSchema.safeParse(data)
         if (!result.success) {
@@ -19,7 +24,15 @@ export default function AddProductForm({ children }: { children: React.ReactNode
             })
             return
         }
-        console.log(result.data)
+        const response = await createProduct(result.data)
+        if (response?.errors) {
+            response.errors.forEach(issue => {
+                toast.error((issue.message))
+            })
+            return
+        }
+        toast.success('Producto creado correctamente')
+        router.push('/admin/products')
     }
 
     return (
