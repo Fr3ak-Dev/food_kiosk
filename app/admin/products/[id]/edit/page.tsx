b9af0f9449e779f280d51fea232831b782f3d7cc
gustavo <kg.2k.pl@gmail.com>
2025-08-26 23:35:42 -0400

    feat(admin): add back button and show current image in product edit form

--- app/admin/products/[id]/edit/page.tsx
+++ app/admin/products/[id]/edit/page.tsx
@@ -1,8 +1,9 @@
-import EditProductForm from "@/components/products/EditProductForm";
-import ProductForm from "@/components/products/ProductForm";
-import Heading from "@/components/ui/Heading";
-import { prisma } from "@/src/lib/prisma";
-import { notFound } from "next/navigation";
+import EditProductForm from "@/components/products/EditProductForm"
+import ProductForm from "@/components/products/ProductForm"
+import GoBackButton from "@/components/ui/GoBackButton"
+import Heading from "@/components/ui/Heading"
+import { prisma } from "@/src/lib/prisma"
+import { notFound } from "next/navigation"
 
 async function getProductById(id: number) {
     const product = await prisma.product.findUnique({
@@ -22,6 +23,7 @@ export default async function EditProductsPage({ params }: { params: { id: strin
     return (
         <>
             <Heading>Editar Producto: {product.name}</Heading>
+            <GoBackButton />
             <EditProductForm>
                 <ProductForm product={product} />
             </EditProductForm>
