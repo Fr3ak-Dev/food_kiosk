c86707424f699a8daf5e16bb43e2f8367e7e100b
gustavo <kg.2k.pl@gmail.com>
2025-08-28 23:52:17 -0400

    feat(order): Integrate SWR to display requested orders in real time

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -1,31 +1,26 @@
+"use client"
+
 import OrderCard from "@/components/order/OrderCard";
 import Heading from "@/components/ui/Heading";
-import { prisma } from "@/src/lib/prisma";
-import { revalidatePath } from "next/cache";
+import { OrderWithProducts } from "@/src/types";
+import useSWR from "swr";
 
-async function getPendingOrders() {
-    
-}
+export default function OrdersPage() {
 
-export default async function OrdersPage() {
+    const url = '/admin/orders/api'
+    const fetcher = () => fetch(url).then(res => res.json()).then(data => data)
+    const { data, error, isLoading } = useSWR<OrderWithProducts[]>(url, fetcher, {
+        refreshInterval: 1000,
+        revalidateOnFocus: false
+    })
 
-    const orders = await getPendingOrders()
-    const refreshOrders = async () => {
-        "use server"
-        revalidatePath('/admin/orders')
-    }
-
-    return (
+    if (data) return (
         <>
             <Heading>Administrar Ordenes</Heading>
-            <form action={refreshOrders}>
-                <input type="submit" value='Actualizar ordenes'
-                    className="bg-amber-400 w-full lg:w-auto text-xl px-10 py-3 text-center
-                        font-bold cursor-pointer"/>
-            </form>
-            {orders.length ? (
+
+            {data.length ? (
                 <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-5 mt-5">
-                    {orders.map(order => (
+                    {data.map(order => (
                         <OrderCard key={order.id} order={order} />
                     ))}
                 </div>
